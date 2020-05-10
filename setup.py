e8115ed58dcb649a06e0f4fc952a2d19b1f4a062
Hassan Kibirige <has2k1@gmail.com>
2020-05-10 22:53:42 +0300

    MAINT: Drop Python 2, update .travis.yml
    - Make setup.py look like plotnine, plydata, mizani
      (for familiarity)

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
-import sys
 import io
-import os
-import json
-import pkgutil
-
-from distutils import log
-from distutils.command.install import install
 from setuptools import find_packages, setup
 
+
+__author__ = 'Hassan Kibirige'
+__email__ = 'has2k1@gmail.com'
+__description__ = 'A gnuplot kernel for Jupyter'
+__license__ = 'BSD'
+__url__ = 'https://github.com/has2k1/gnuplot_kernel'
+__classifiers__ = [
+    'Framework :: IPython',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: System :: Shells',
+]
+__install_requires__ = [
+    'metakernel >= 0.20.14',
+    'notebook >= 5.5.0'
+]
+__packages__ = find_packages(
+    include=['gnuplot_kernel', 'gnuplot_kernel.*']
+)
+__package_data__ = {'gnuplot_kernel': ['images/*.png']}
 
 with io.open('gnuplot_kernel/kernel.py', encoding='utf-8') as fid:
     for line in fid:
@@ -19,28 +35,17 @@ with open('README.rst') as f:
     readme = f.read()
 
 setup(name='gnuplot_kernel',
+      author=__author__,
+      maintainer=__author__,
+      maintainer_email=__email__,
       version=__version__,
-      description='A gnuplot kernel for Jupyter',
+      description=__description__,
       long_description=readme,
-      author='Hassan Kibirige',
-      author_email='has2k1@gmail.com',
-      license='BSD',
-      url="https://github.com/has2k1/gnuplot_kernel",
-      install_requires=[
-          'metakernel >= 0.20.14',
-          'notebook >= 5.4.0'],
-      packages=find_packages(
-          include=['gnuplot_kernel', 'gnuplot_kernel.*']
-      ),
-      package_data={'gnuplot_kernel': ['images/*.png']},
-      classifiers=[
-          'Framework :: IPython',
-          'Intended Audience :: End Users/Desktop',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-          'Topic :: Scientific/Engineering :: Visualization',
-          'Topic :: System :: Shells',
-      ]
+      license=__license__,
+      url=__url__,
+      python_requires='>=3.6',
+      install_requires=__install_requires__,
+      packages=__packages__,
+      package_data=__package_data__,
+      classifiers=__classifiers__
       )
